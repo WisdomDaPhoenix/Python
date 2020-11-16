e1bf55f52588d26ba5ea0e0cd2fb4965aecff630
WisdomDaPhoenix <74541786+WisdomDaPhoenix@users.noreply.github.com>
2020-11-16 14:25:58 +0100

    Add files via upload

---
+++ Temperature.py
@@ -0,0 +1,6 @@
+#temp = eval(input('Enter a temperature in Celsius: '))
+#print('In Fahrenheit, that is', 9/5*temp+32)
+
+
+temp = eval(input('Enter temperature in celsius:'));
+print('In Fahrenheit, the temperature is', (9/5*temp)+32);
